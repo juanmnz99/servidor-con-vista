27a1606903947d2c127f1bb5f668fb2b49263873
segundaEntrega <juanunezaa@gmail.com>
2023-06-12 01:26:50 -0300

    autenticacion

--- models/User.js
+++ models/User.js
@@ -20,3 +20,21 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+const bcrypt = require('bcrypt');
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
+});
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,3 +14,81 @@ router.post('/login', (req, res) => {
 });
 
 module.exports = router;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return done(null, false, { message: 'Usuario no encontrado' });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return done(null, false, { message: 'Contraseña incorrecta' });
+    }
+
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+}));
+
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8080/auth/github/callback'
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    
+    let user = await User.findOne({ githubId: profile.id });
+
+    if (user) {
+      return done(null, user);
+    }
+
+    user = new User({
+      username: profile.username,
+      githubId: profile.id
+    });
+
+    await user.save();
+
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/products',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.get('/github', passport.authenticate('github'));
+
+router.get('/github/callback', passport.authenticate('github', {
+  successRedirect: '/products',
+  failureRedirect: '/login'
+}));
